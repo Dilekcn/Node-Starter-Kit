d9e543d55213f2da12147588b12b8caaf06bdb58
Dilekcn <dcanturk1@gmail.com>
2021-11-15 13:54:38 +0000

    post

--- server.js
+++ server.js
@@ -34,6 +34,12 @@ app.get("/json", function (req, res) {
     res.send(`You searched for Lat: ${lat} and Lng: ${lng}`);
 });
 //http://localhost:3000/json?lat=51.5311&lng=0.0481
+
+app.post('/', function (req, res) {
+    res.send('Got a POST request')
+  })
+
+  
 app.listen(3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
